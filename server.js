2659f8c82ea4943b7bb109e8c2a144866f49e1b2
firdan <onpeci@gmail.com>
2025-04-03 18:23:42 +0700

    clear text error

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ export default function(opt) {
         const client = manager.getClient(clientId);
         if (!client) {
             res.statusCode = 404;
-            res.end('404');
+            res.end('404 - client not available');
             return;
         }
 
